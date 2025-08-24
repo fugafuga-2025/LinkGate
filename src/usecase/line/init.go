8c1c76ce4a830275ede1bd73b61a2c816d917e27
matsu <c0b211447a@gmail.com>
2025-08-24 13:15:21 +0000

    fix: LINE各種トークンとグループIDを.envへ移行

--- src/usecase/line/init.go
+++ src/usecase/line/init.go
@@ -4,17 +4,23 @@ import (
 	"fmt"
 	"fuagfuga-2025-LinkGate/src/model"
 	"log"
+	"os"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-var channelSecret = "a37672fceb5a6e39d7ed4aee215bfc9d"
-var channelToken = "1cLQiY/aumnpIGCWg2Eme3r3to6UNhdFhFtYveh0d8P43ZcB10B+GxGfQrbV5Bqck7PKD6kwAKjoQNkdPFriu2hgjTMoTy727Ib+lIHVpu2gU+y8v7cCL4JX4a56zvaEAWX99jlfBS/l47kF+ZA8WwdB04t89/1O/w1cDnyilFU="
-var testGroupID = "C56e7053ee8c56dbd99e94964216ce69c"
+// LINEbotの作成時に必要となる各種トークンの取得
+var channelSecret = os.Getenv("LINE_CHANNEL_SECRET")
+var channelToken = os.Getenv("LINE_CHANNEL_TOKEN")
+
+// メッセージ送信先となるLINEグループのID
+var groupID = os.Getenv("LINE_GROUP_ID")
+
 var bot *linebot.Client
 
 func createBot() {
 	var err error
+
 	bot, err = linebot.New(channelSecret, channelToken)
 	if err != nil {
 		fmt.Printf("LINE botの生成に失敗しました:%s", err)
@@ -23,15 +29,14 @@ func createBot() {
 
 func CreateLINEMessage(msg model.Message) {
 	createBot()
+
+	// メッセージ送信者、内容、送信元プラットフォームを取得
+	userName := msg.User.Name
 	text := msg.Content.Text
 	platform := msg.User.Platform
-	userName := msg.User.Name
 
-	sendText := fmt.Sprintf("from: %sさん\n\n%s\n\n(Platform: %s)", userName, text, platform)
-	log.Print(sendText)
-
-	lineText := linebot.NewTextMessage(sendText)
-	if _, err := bot.PushMessage(testGroupID, lineText).Do(); err != nil {
+	lineText := linebot.NewTextMessage(fmt.Sprintf("from: %sさん\n\n%s\n\n(Platform: %s)", userName, text, platform))
+	if _, err := bot.PushMessage(groupID, lineText).Do(); err != nil {
 		if apiErr, ok := err.(*linebot.APIError); ok {
 			fmt.Printf("LINE API Error: code=%d message=%s\n", apiErr.Code, apiErr.Response.Message)
 
@@ -42,6 +47,6 @@ func CreateLINEMessage(msg model.Message) {
 			fmt.Println("Other error:", err)
 		}
 	} else {
-		log.Printf("LINE送信成功: %s", lineText.Text)
+		log.Println("LINE送信成功")
 	}
 }
